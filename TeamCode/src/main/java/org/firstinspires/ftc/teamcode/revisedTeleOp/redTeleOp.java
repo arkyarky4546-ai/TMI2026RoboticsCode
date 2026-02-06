af0274c5d4fbc1ea8d84f18a60c5c8a8fb60a4dd
Noah Kitayama <arkyarky4546@gmail.com>
2026-02-05 20:18:01 -0600

    noah is very smart

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/revisedTeleOp/redTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/revisedTeleOp/redTeleOp.java
@@ -81,6 +81,8 @@ public class redTeleOp extends OpMode {
         }
         telemetry.addData("rightTrigger", rightTrigger);
         telemetry.addData("shootfirst", shootFirst);
+        telemetry.addData("drivetrain.getDistanceFromGoal()", drivetrain.getDistanceFromGoal());
+        telemetry.addData("hood", shooterAndIntake.shooterHood.getPosition());
         shooterAndIntake.update(drivetrain.getDistanceFromGoal(), leftTrigger ,rightTrigger, gamepad2.left_bumper, gamepad2.dpadUpWasPressed(),telemetry);
         //shooterAndIntake.update(drivetrain.getDistanceFromGoal(), leftTrigger, rightTrigger, gamepad2.left_bumper, gamepad2.right_bumper, gamepad2.x, gamepad2.b, gamepad2.y, gamepad1.dpadRightWasPressed(), gamepad1.dpadUpWasPressed(), telemetry);
 
@@ -106,7 +108,7 @@ public class redTeleOp extends OpMode {
         }*/
     }
     @Override
-    public void start(){
+    public void stop(){
         shooterAndIntake.stopT();
     }
 }
