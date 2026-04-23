a06f310a0c1879031ac99342083be082026c889a
Noah Kitayama <arkyarky4546@gmail.com>
2026-04-23 15:46:37 -0500

    asdasdlkj

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/SkeletonAuto.java
@@ -0,0 +1,186 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.AutoTurret;
+import org.firstinspires.ftc.teamcode.LimeLight;
+import org.firstinspires.ftc.teamcode.intakeShoot;
+
+@Autonomous(name = "SkeletonAuto", group = "Autonomous")
+public class SkeletonAuto extends OpMode {
+    private Follower follower; //this guy just kinda executes the paths type stuff yk
+    private Timer pathTimer, actionTimer, opmodeTimer; //Path timer can be used in the autonomousPathUpdate just to see if one of the paths failed or something
+
+    //positions
+    private int pathState; //just an int used later in autonomousPathUpdate for each of the cases (tells which path to do)
+    private final Pose startPose = new Pose(128.8,-24.5, -2.35720214); // Start Pose of our robot. (I think these are the right measurements, as 0 degrees corresponds to facing right the starting x is a bit weird as it depends on where on the line we start)
+
+    //paths
+    private Path score1;
+    private PathChain firstLoad;
+    private double IntegralSum = 0;
+    private double lastError = 0;
+    public static double Kp=0.0121;
+    public static double Ki=0.00014;
+    public static double Kd=0.0000;
+    public static double Kf=.0000;
+    //double savePosition = 0.0;
+
+    //timer
+    private ElapsedTime shootTimer = new ElapsedTime();
+    ElapsedTime PIDtimer=new ElapsedTime();
+
+    //custom class that controls shooting and intake
+    private intakeShoot intakeAndShoot;
+
+    //servos
+    Servo push, turretRight, turretLeft, hood;
+
+    //booleans
+    boolean intakeIndex = true;
+    boolean isShoot = false;
+    boolean go = false;
+    private Boolean scan = true;
+    boolean gate = true;
+    //boolean intake = false;
+    //boolean shoot = false;
+    AutoTurret turret;
+    //ints
+    //int index = 0;
+    //int shootPos = 1;
+    int intakePos = 0;
+    LimeLight Lime;
+    //int[] pattern = {1,2,2};
+    //int[] ppg = {2,2,1};
+    //int[] pgp = {2,1,2};
+    //int[] gpp = {1,2,2};
+    boolean auto = false;
+
+
+    public double PIDControl(double reference, double state){
+        double error=reference-state;
+        double dt = PIDtimer.seconds();
+        IntegralSum+=error*dt;
+        double derivative=(error-lastError)/dt;
+        lastError=error;
+        PIDtimer.reset();
+        return (error*Kp)+(derivative*Kd)+(IntegralSum*Ki)+(reference*Kf);
+    }
+    public void buildPaths() {//this is where we build the path stuff using our positions
+        score1 = new Path(new BezierLine(startPose, scorePose1));
+        score1.setLinearHeadingInterpolation(startPose.getHeading(), scorePose1.getHeading());
+
+       end = follower.pathBuilder()
+                .addPath(new BezierLine(scorePose1,endPose1))
+                .setLinearHeadingInterpolation(scorePose1.getHeading(), endPose1.getHeading())
+                //  .setHeadingConstraint(.95)
+                //  .setTranslationalConstraint(.95)
+                .build();
+    }
+    public void autonomousPathUpdate() throws InterruptedException {//we can add a lot more paths
+        switch (pathState) {
+
+            case 0:
+                //the follower is now on the score1 path
+                follower.followPath(score1);
+                intakeAndShoot.setPos(0,intakePos);
+                intakeIndex = false;
+                isShoot = false;
+                //go to the next case
+                setPathState(1);
+
+                break;
+
+        }
+    }
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
+    /*public double shooterPowerSet(){
+        double distanceFromGoal = Math.pow((Math.pow((144-follower.getPose().getX()),2) + Math.pow((144 + follower.getPose().getY()),2)) , .5);
+        return 0.0000145 * Math.pow(distanceFromGoal, 4) - 0.00584813 * Math.pow(distanceFromGoal, 3) + 0.834897 * Math.pow(distanceFromGoal, 2) - 45.38315 * Math.pow(distanceFromGoal, 1) + 1930.07059;
+    }
+    public double hoodPosSet(){
+        double distanceFromGoal = Math.pow((Math.pow((144-follower.getPose().getX()),2) + Math.pow((144+ follower.getPose().getY()),2)) , .5);
+        return  -Math.pow(10, -9) * 2.0571 * Math.pow(distanceFromGoal, 4) - Math.pow(10, -7)*8.57305 * Math.pow(distanceFromGoal, 3) + 0.000313995 * Math.pow(distanceFromGoal, 2) - 0.0237158 * Math.pow(distanceFromGoal, 1) + 0.88;
+    }
+    public double getRecoil(){
+        double distanceFromGoal = Math.pow((Math.pow((144-follower.getPose().getX()),2) + Math.pow((144 + follower.getPose().getY()),2)) , .5);
+        return  -Math.pow(10, -9) * 5.66719 * Math.pow(distanceFromGoal, 4) + 0.00000199279 * Math.pow(distanceFromGoal, 3) -0.00024284 * Math.pow(distanceFromGoal, 2) +0.0127555 * Math.pow(distanceFromGoal, 1) -0.1900;
+    }*/
+    @Override
+    public void loop() { //this runs constantly during auto and we just update the position of the follower and check if it is still busy and cycle through each case
+
+        follower.update();
+        intakeAndShoot.update1(false,false, false, isShoot, false, follower, telemetry, auto,false);
+        if(!auto) {
+            intakeAndShoot.intakesetPower(1);
+        }
+
+        //intakeAndShoot.update(1, pathState, telemetry, intakeIndex); //updating our shooter power and intake power
+        try {
+            autonomousPathUpdate(); //updating our cases so that we can change paths
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+
+        }
+
+
+        //telemetry debugs and current position tracking
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+
+        telemetry.update();
+    }
+
+    @Override
+    public void init() {
+        pathTimer = new Timer();
+        actionTimer = new Timer();
+        opmodeTimer = new Timer();
+        opmodeTimer.resetTimer();
+        follower = Constants.createFollower(hardwareMap);
+
+        follower.setStartingPose(startPose);
+        intakeAndShoot = new intakeShoot(hardwareMap,"intake", "intake1",
+                "shoot1", "shoot2",
+                "spindexRoter", "slave"
+                , "wally", "color1", "color2", "shooterHood", follower);
+        intakeAndShoot.setModeBlue();
+        hood = hardwareMap.get(Servo.class, "shooterHood");
+        turretRight = hardwareMap.get(Servo.class, "turretRight");
+        turretLeft = hardwareMap.get(Servo.class, "turretLeft");
+        turret = new AutoTurret(hardwareMap, "turretLeft", "turretRight");
+        turret.setModeBlue();
+        Lime = new LimeLight(hardwareMap);
+        buildPaths();
+    }
+
+
+
+    @Override
+    public void init_loop() {}
+
+    @Override
+    public void start() {
+        opmodeTimer.resetTimer();
+        setPathState(0);
+    }
+
+    @Override
+    public void stop() {
+        intakeAndShoot.stopT();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/revisedTeleOp/SkeletonTeleop.java
@@ -0,0 +1,243 @@
+package org.firstinspires.ftc.teamcode.revisedTeleOp;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+// Import your new move-shoot classes
+import org.firstinspires.ftc.teamcode.AutoTurret;
+import org.firstinspires.ftc.teamcode.Shooter;
+import org.firstinspires.ftc.teamcode.ShooterConstants;
+import org.firstinspires.ftc.teamcode.shooterThread;
+import org.firstinspires.ftc.teamcode.LimeLight;
+import org.firstinspires.ftc.teamcode.intakeShoot;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Configurable
+@TeleOp
+public class SkeletonTeleop extends OpMode {
+
+
+
+    // --- ROBOT SUBSYSTEMS ---
+    Drivetrain drivetrain;
+    AutoTurret turret;
+    intakeShoot shooterAndIntake;
+    Limelight3A limelight3A;
+    LimeLight Lime;
+
+
+    Shooter shooterCalculator;
+    //shooterThread shootThread;
+    private double greenPos = 0.0;
+
+    boolean shootFirst = true;
+    int index = 1;
+    boolean aim = true;
+    int[] pattern = {1,2,2};
+    int[] ppg = {2,2,1};
+    int[] pgp = {2,1,2};
+    int[] gpp = {1,2,2};
+    int[][] entire = {ppg, pgp, gpp};
+    private boolean reset = true;
+    private int patternIndex = 0;
+    private boolean jorkIt = false;
+    private boolean sort = false;
+    private boolean sorting = false;
+    private ElapsedTime shootTimer = new ElapsedTime();
+    private ElapsedTime spin = new ElapsedTime();
+    private boolean patternFound = false;
+    private Servo turretRight;
+    boolean slow = false;
+
+    @Override
+    public void init() {
+        drivetrain = new Drivetrain(hardwareMap);
+        drivetrain.setModeBlue();
+        Lime = new LimeLight(hardwareMap);
+        turretRight = hardwareMap.get(Servo.class, "turretRight");
+        // Initialize the new AutoTurret
+        turret = new AutoTurret(hardwareMap, "turretLeft", "turretRight");
+        turret.setModeBlue();
+
+        limelight3A = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight3A.pipelineSwitch(6);
+        limelight3A.start();
+
+        shooterAndIntake = new intakeShoot(hardwareMap,"intake", "intake1",
+                "shoot1", "shoot2",
+                "spindexRoter", "slave",
+                "wally", "color1", "color2", "shooterHood", drivetrain.getFollower());
+        shooterAndIntake.setModeBlue();
+
+        shooterCalculator = new Shooter();
+        double initialHeading = drivetrain.getFollower().getPose().getHeading();
+        //shootThread = new shooterThread(shooterCalculator, drivetrain.getFollower(), ShooterConstants.GOAL_POSE_BLUE, initialHeading);
+        //shootThread.start();
+
+        //shooterAndIntake.wallPos(0.2);
+
+    }
+
+    @Override
+    public void loop() {
+        // --- DRIVETRAIN ---
+        drivetrain.update(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.xWasPressed(), gamepad2.bWasPressed(), gamepad1.yWasPressed(), gamepad2.dpadRightWasPressed(), gamepad2.dpadLeftWasPressed(), gamepad1.dpad_left, slow);
+
+        if(gamepad1.a){
+            drivetrain.resetCurrentPose();
+        }
+        if(gamepad1.b){
+            drivetrain.resetCurrentPoseGoal();
+        }
+        if(gamepad2.yWasPressed()){
+            sort = true;
+            greenPos = shooterAndIntake.getGreen();
+        }
+        if(gamepad2.a){
+            sort = true;
+            sorting = true;
+            reset = false;
+            if(spin.milliseconds() < 500) {
+                shooterAndIntake.slow(index);
+            }
+            else{
+                spin.reset();
+                if(index > 5){
+                    index = 0;
+                }
+                else{
+                    index +=1;
+                }
+            }
+        }
+        if(gamepad2.xWasPressed()){
+            //sort  = false;
+            reset = false;
+            if(sorting){
+                sorting = false;
+                shootTimer.reset();
+            }
+            shooterAndIntake.colorSort(greenPos, pattern);
+
+
+        }
+       /* if (!sorting && shootTimer.milliseconds() > 1000 && shootTimer.milliseconds() < 1050){
+            shooterAndIntake.colorSort(greenPos, pattern);
+        }*/
+
+        double currentHeading = drivetrain.getFollower().getPose().getHeading();
+        //  shootThread.update(drivetrain.getFollower(), ShooterConstants.GOAL_POSE_BLUE, currentHeading);
+
+       /* double dynamicTurretAngle = shootThread.getTurretPos();
+        double dynamicHoodPos = shootThread.getHoodPos();
+        double dynamicFlywheelSpeed = shootThread.getSpeed();*/
+
+        // Pass the dynamically calculated angle into the turret
+        if(aim){
+            turret.updateAuto(drivetrain.getFollower(), telemetry, shooterAndIntake.turretAngle(), aim);
+        }
+
+        if(gamepad2.dpadDownWasPressed()){
+            aim = !aim;
+        }
+        if(!patternFound){
+            if(Lime.getPatternFromLimelight() == 0){
+                pattern = gpp;
+                patternFound = true;
+            } else if(Lime.getPatternFromLimelight() == 1){
+                pattern = pgp;
+                patternFound = true;
+            } else if(Lime.getPatternFromLimelight() == 2){
+                pattern = ppg;
+                patternFound = true;
+            }
+        }
+        else if(gamepad1.dpadRightWasPressed()){
+            slow = !slow;
+        }
+
+        boolean leftTrigger = false;
+        boolean rightTrigger = false;
+
+        boolean gateLeftBumper = gamepad1.left_bumper;
+        if(gamepad2.leftBumperWasPressed()){
+            if(patternIndex >2){
+                patternIndex  = 0;
+            }
+            else{
+                patternIndex += 1;
+            }
+            pattern = entire[patternIndex];
+        }
+
+        boolean rightBumper = gamepad2.right_bumper;
+
+
+        //Check for Shooting (Highest Priority)
+        if(gamepad1.right_trigger > 0.75){
+            reset = true;
+            rightTrigger = true;
+            shootFirst = false;
+            sort = false;
+
+            // Feed the dynamically calculated velocity instead of the static TargetVelocity
+            //shooterAndIntake.shootsetVelocity(dynamicFlywheelSpeed);
+
+            // Override and disable intake while shooting
+
+        } else {
+            // Not Shooting (Allow Intake and Default States)
+            shootFirst = true;
+            //shooterAndIntake.wallPos(.2127);
+            if(reset) {
+                //shooterAndIntake.setPos(0, 0);
+            }
+            // Only allow intake if the right trigger is NOT pressed
+            if(gamepad1.left_trigger > 0.75){
+                reset = true;
+                leftTrigger = true;
+            }
+        }
+
+        //  shooterAndIntake.hoodPos(dynamicHoodPos);
+
+        shooterAndIntake.update(leftTrigger, false, gateLeftBumper, rightTrigger, rightBumper, drivetrain.getFollower(), telemetry, sort, gamepad2.dpad_up, false);
+
+
+        telemetry.addData("RightTrigger (Shooting)", rightTrigger);
+        telemetry.addData("sort", sort);
+        telemetry.addData("turret angle", turretRight.getPosition());
+        //telemetry.addData("ShootFirst", shootFirst)
+        //    telemetry.addData("Target Velocity (Dynamic)", dynamicFlywheelSpeed);
+        telemetry.addData("Current Velocity", shooterAndIntake.getVelocity());
+        //  telemetry.addData("Dynamic Hood Pos", dynamicHoodPos);
+        telemetry.addData("Distance", drivetrain.getDistanceFromGoal());
+        //telemetry.addData("Calculated Turret Angle", dynamicTurretAngle);
+        telemetry.addData("Debug Mode", rightBumper);
+        // telemetry.addData("BL Vertex Has Ball", shooterAndIntake.getBLBallState());
+        //  telemetry.addData("ST Vertex Has Ball", shooterAndIntake.getSTBallState());
+        telemetry.addData("Pattern Found: ", patternFound);
+        if(pattern[0] == ppg[0] && pattern[1] == ppg[1] && pattern[2] == ppg[2]){
+            telemetry.addData("Pattern: ppg", 0);
+        }
+        if(pattern[0] == pgp[0] && pattern[1] == pgp[1] && pattern[2] == pgp[2]){
+            telemetry.addData("Pattern: pgp", 0);
+        }
+        if(pattern[0] == gpp[0] && pattern[1] == gpp[1] && pattern[2] == gpp[2]) {
+            telemetry.addData("Pattern: gpp", 0);
+        }
+        telemetry.addData("greenPos: ", greenPos);
+    }
+
+    @Override
+    public void stop(){
+        // Crucial: Kill the background thread to prevent memory leaks or crashes after stopping the OpMode
+        //shootThread.stopThread();
+        shooterAndIntake.stopT();
+
+    }
+}
